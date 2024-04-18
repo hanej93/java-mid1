5fe125b904afd4acf40f255618f8095d71de2663
EJHan <hanej93@naver.com>
2024-04-18 09:48:14 +0900

    java.lang 패키지 소개

---
+++ src/lang/LangMain.java
@@ -0,0 +1,11 @@
+package lang;
+
+// 너무 자주 사용하는 패키지이기 때문에 생략가능!
+// import java.lang.System;
+
+public class LangMain {
+
+	public static void main(String[] args) {
+		System.out.println("hello java");
+	}
+}
